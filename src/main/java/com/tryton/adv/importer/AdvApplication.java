ec0c56bb1c173d005c4acdab81083e3e8ff03b69
mephisto2120 <mephisto2120@gmail.com>
2021-07-09 08:01:14 +0200

    Setup project

---
+++ src/main/java/com/tryton/adv/importer/AdvApplication.java
@@ -0,0 +1,11 @@
+package com.tryton.adv.importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvApplication.class, args);
+    }
+}
