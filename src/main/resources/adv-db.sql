8785edb98f8707ccd9d7d4d496ce0ce7750ccfef
mephisto2120 <mephisto2120@gmail.com>
2021-07-11 19:37:56 +0200

    SC# Renaming db, configuring profiles

--- src/main/resources/adv-db.sql
+++ src/main/resources/adv-db.sql
@@ -5,50 +5,50 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema adv_db
+-- Schema adv-db
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema adv_db
+-- Schema adv-db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `adv_db` DEFAULT CHARACTER SET utf8 ;
-USE `adv_db` ;
+CREATE SCHEMA IF NOT EXISTS `adv-db` DEFAULT CHARACTER SET utf8 ;
+USE `adv-db` ;
 
 -- -----------------------------------------------------
--- Table `adv_db`.`datasource`
+-- Table `adv-db`.`datasource`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `adv_db`.`datasource` (
+CREATE TABLE IF NOT EXISTS `adv-db`.`datasource` (
     `ds_id` BIGINT(20) NOT NULL,
     `ds_name` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`ds_id`))
     ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `ds_name_UNIQUE` ON `adv_db`.`datasource` (`ds_name` ASC) VISIBLE;
+CREATE UNIQUE INDEX `ds_name_UNIQUE` ON `adv-db`.`datasource` (`ds_name` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `adv_db`.`campaign`
+-- Table `adv-db`.`campaign`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `adv_db`.`campaign` (
+CREATE TABLE IF NOT EXISTS `adv-db`.`campaign` (
     `cpg_id` BIGINT(20) NOT NULL,
     `cpg_name` VARCHAR(255) NOT NULL,
     `cpg_ds_id` BIGINT(20) NOT NULL,
     PRIMARY KEY (`cpg_id`),
     CONSTRAINT `fk_campaign_datasource`
     FOREIGN KEY (`cpg_ds_id`)
-    REFERENCES `adv_db`.`datasource` (`ds_id`)
+    REFERENCES `adv-db`.`datasource` (`ds_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-CREATE UNIQUE INDEX `c_name_UNIQUE` ON `adv_db`.`campaign` (`cpg_ds_id` ASC, `cpg_name` ASC) VISIBLE;
+CREATE UNIQUE INDEX `c_name_UNIQUE` ON `adv-db`.`campaign` (`cpg_ds_id` ASC, `cpg_name` ASC) VISIBLE;
 
-CREATE INDEX `fk_campaign_datasource_idx` ON `adv_db`.`campaign` (`cpg_ds_id` ASC) VISIBLE;
+CREATE INDEX `fk_campaign_datasource_idx` ON `adv-db`.`campaign` (`cpg_ds_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `adv_db`.`metrics`
+-- Table `adv-db`.`metrics`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `adv_db`.`metrics` (
+CREATE TABLE IF NOT EXISTS `adv-db`.`metrics` (
     `met_id` BIGINT(20) NOT NULL,
     `met_clicks` INT NOT NULL,
     `met_impressions` INT NOT NULL,
@@ -57,11 +57,11 @@ CREATE TABLE IF NOT EXISTS `adv_db`.`metrics` (
     PRIMARY KEY (`met_id`),
     CONSTRAINT `fk_metrics_campaign1`
     FOREIGN KEY (`met_cpg_id`)
-    REFERENCES `adv_db`.`campaign` (`cpg_id`)
+    REFERENCES `adv-db`.`campaign` (`cpg_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-CREATE INDEX `fk_metrics_campaign1_idx` ON `adv_db`.`metrics` (`met_cpg_id` ASC) VISIBLE;
+CREATE INDEX `fk_metrics_campaign1_idx` ON `adv-db`.`metrics` (`met_cpg_id` ASC) VISIBLE;
 
 CREATE SEQUENCE IF NOT EXISTS `cpg_seq`;
 CREATE SEQUENCE IF NOT EXISTS `ds_seq`;
