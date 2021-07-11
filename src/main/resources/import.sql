f4fe4087b8d9d0dca3644f775548dd3dcf75d3ec
mephisto2120 <mephisto2120@gmail.com>
2021-07-11 20:37:14 +0200

    Configuring swagger and db initialization by hibernate

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema adv-db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `adv-db` DEFAULT CHARACTER SET utf8 ;
+-- CREATE SCHEMA IF NOT EXISTS `adv-db` DEFAULT CHARACTER SET utf8 ;
 USE `adv-db` ;
 
 -- -----------------------------------------------------
