455396234a4d375ba6679e65f85f053cdce136a0
mephisto2120 <mephisto2120@gmail.com>
2022-06-13 15:25:27 +0200

    Using ConfigurationProperties

--- src/main/java/com/tryton/adv/importer/config/DataSourceProperties.java
+++ src/main/java/com/tryton/adv/importer/config/DataSourceProperties.java
@@ -2,27 +2,20 @@ package com.tryton.adv.importer.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
 public class DataSourceProperties {
-    @Value("${spring.datasource.url}")
     private String url;
-    @Value("${spring.datasource.username}")
     private String username;
-    @Value("${spring.datasource.password}")
     private String password;
-    @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
-    @Value("${spring.datasource.tomcat.initial-size}")
     private int initialSize;
-    @Value("${spring.datasource.tomcat.max-wait}")
     private int maxWait;
-    @Value("${spring.datasource.tomcat.max-active}")
     private int maxActive;
-    @Value("${spring.datasource.tomcat.max-idle}")
     private int maxIdle;
-    @Value("${spring.datasource.tomcat.min-idle}")
     private int minIdle;
 }
