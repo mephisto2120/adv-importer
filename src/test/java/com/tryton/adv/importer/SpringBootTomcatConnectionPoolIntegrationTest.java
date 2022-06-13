55e8c08e0f12b4cd8855d5a415d956bb4ba3c363
mephisto2120 <mephisto2120@gmail.com>
2022-06-13 15:09:13 +0200

    Fixing test

--- src/test/java/com/tryton/adv/importer/SpringBootTomcatConnectionPoolIntegrationTest.java
+++ src/test/java/com/tryton/adv/importer/SpringBootTomcatConnectionPoolIntegrationTest.java
@@ -3,12 +3,14 @@ package com.tryton.adv.importer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SpringBootTomcatConnectionPoolIntegrationTest {
 
     @Autowired
